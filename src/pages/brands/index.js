62124058b62ada5b9f7123b34244691354fbf478
yangfei19881024 <yangfei@boqii.com>
2020-11-06 22:38:34 +0800

    修复bug

--- src/pages/brands/index.js
+++ src/pages/brands/index.js
@@ -17,7 +17,7 @@ $(function(){
     var datas_ary = [tech_finance, consume, healthy, city_con, energy_services]
 
     // xmlLoadProcess(datas_ary[index])
-    // $('#houseName').hide()
+    $('#houseName').hide()
     XML_DATA = new xmlLoadProcess(datas_ary[index]);
     $('#circle3d').empty()
     circle3d_firstStart()
